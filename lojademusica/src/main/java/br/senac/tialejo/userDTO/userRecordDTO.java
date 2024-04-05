2293de73e9d7e41e2eb775e17b56dfb58bff3550
RafaMachad <rmr.eu@hotmail.com.br>
2024-04-05 15:14:36 -0300

    V0.0.19
    +Organização de hiperlinks
    +roteamento para landing Page

--- lojademusica/src/main/java/br/senac/tialejo/userDTO/userRecordDTO.java
+++
@@ -1,6 +0,0 @@
-package br.senac.tialejo.userDTO;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record userRecordDTO(@NotBlank String name,@NotBlank String email, Integer telefone, String grupo, String status) {
-}
